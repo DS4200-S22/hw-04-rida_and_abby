b5b231f321f64d95cd06d502a5f873e5f3523534
ridasharief <sharief.r@northeastern.edu>
2022-02-12 14:20:43 -0500

    circle 2 mod

--- main_hw.js
+++ main_hw.js
@@ -3,12 +3,20 @@
 // times this svg has been clicked on. Each time the svg
 // clicked on, the count should update.
 
-var counter = 0
+var counter = 0;
 
 function circle1_click() {
 
-	counter += 1
+	counter += 1;
 
 	document.getElementById("circle1text").innerHTML = counter;
+
+}
+
+function circle2_mouseover() {
+
+	document.getElementById("circle2").stroke = black;
+	document.getElementById("circle2").stroke-width = 3px;
+
 
 }
