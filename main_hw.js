4499316e90df3f561c4ad2be15f34b3653b7de30
ridasharief <sharief.r@northeastern.edu>
2022-02-12 14:01:30 -0500

    counter trial

---
+++ main_hw.js
@@ -0,0 +1,12 @@
+
+// Add text in the center of the circle that shows how many 
+// times this svg has been clicked on. Each time the svg
+// clicked on, the count should update.
+
+function circle1_click() {
+
+	//let counter = 
+
+	document.getElementById("circle1text").innerHTML++;
+
+}
